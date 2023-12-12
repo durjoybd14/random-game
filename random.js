d2f78570b6aa4fd9657610329d3add2a550a150c
durjoybd14 <nrd141913@gmail.com>
2023-12-12 12:19:40 +0600

    ranom game

---
+++ random.js
@@ -0,0 +1,11 @@
+// const num = 100000000
+// console.log(num.toLocaleString('en-BD'));
+
+const max = 2;
+const min = 1;
+
+const value = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(value);
+
+// console.log((Math.floor(Math.random()*100000000)));
+
